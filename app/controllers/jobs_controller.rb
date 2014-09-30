04e249dd0bd003b76262650cce881bf3b49ef6ad
Josannah Keller <josannahkeller@gmail.com>
2014-09-29 20:47:35 -0400

    first heroku stuff

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,45 @@
+class JobsController < ApplicationController
+before_action :authenticate_user!
+
+	def index
+		@jobs = current_user.jobs
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = current_user.jobs.build(job_params)
+		# @job = Job.new(job_params)
+		# @job.user = current_user
+		@job.save
+		redirect_to jobs_path
+	end
+
+	def edit
+		@job = Job.find(params[:id])
+	end
+
+	def update
+		@job = Job.find(params[:id])
+
+		if @job.update(job_params)
+			redirect_to jobs_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@job = Job.find(params[:id])
+		@job.destroy
+
+		redirect_to jobs_path
+	end
+
+	private
+		def job_params
+			params.require(:job).permit(:company, :title, :url)
+		end
+end
---
+++ app/models/job.rb
@@ -0,0 +1,22 @@
+class Job < ActiveRecord::Base
+	before_save :query_link
+	belongs_to :user
+	require 'net/http'
+
+	def query_link
+		uri = URI(self.url)
+		res = Net::HTTP.get_response(uri)
+		return res.code
+	end
+
+	def res_display(code)
+		if code == "200"
+			return "Active Job"
+		elsif code == "404"
+			return "Inactive Job"
+		else
+			return "Unknown Status"
+		end
+	end
+
+end
